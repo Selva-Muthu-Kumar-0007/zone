dfbe196dbd36bc442400787a0bf024dabd648c1e
Selva Muthu Kumar <selva8822@gmail.com>
2021-07-25 09:30:05 +0530

    adding workflow

--- app/models/zone.rb
+++ app/models/zone.rb
@@ -3,7 +3,7 @@ class Zone < ActiveRecord::Base
     validates :name, uniqueness: { scope: [:vaccount_id], message: 'is already present' }
     validates :slug, uniqueness: { scope: [:vaccount_id], message: 'is already present' }
 
-    before_save :create_slug
+    # before_save :create_slug
 
     STATE = {active: 1, inactive: 2, deleted: 3}
     enum state: STATE
--- db/migrate/20210225074802_create_zones.rb
+++ db/migrate/20210225074802_create_zones.rb
@@ -3,6 +3,9 @@ class CreateZones < ActiveRecord::Migration[5.0]
   def change
     create_table :zones do |t|
       t.string :name
+      t.string :slug
+      t.integer :state
+      t.integer :vaccount_id, default: 1
       t.json :coordinates
       t.datetime :deleted_at
       t.timestamps
